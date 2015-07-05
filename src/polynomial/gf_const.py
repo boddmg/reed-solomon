e710b5dd5beab965ac2f4c58ca021d63995db637
boddmg <boddmg@gmail.com>
2015-07-05 20:50:13 +0800

    Finished the first version of the class GaloisFieldInt.

--- src/polynomial/gf_const.py
+++ src/polynomial/gf_const.py
@@ -4,11 +4,11 @@ x = 1
 for i in range(1,255):
   x <<= 1
   if x & 0x100:
-      x ^= 0x11d
+      x ^= 0x12d
   gf_exp[i] = x
   gf_log[x] = i
 for i in range(255,512):
   gf_exp[i] = gf_exp[i-255]
 
-print "const GF_EXP: [u8; 512] = %s" % gf_exp
-print "const GF_LOG: [u8; 256] = %s" % gf_log
+print "const GF_EXP: [u8; 512] = %s;" % gf_exp
+print "const GF_LOG: [u8; 256] = %s;" % gf_log
